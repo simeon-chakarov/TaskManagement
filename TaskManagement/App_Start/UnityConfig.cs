e8ee6596537e28cad5477217e14042214fe27d77
Simeon Chakarov <simeon.tchakarov.github@gmail.com>
2022-08-16 19:17:57 +0300

    Fix broken registration.

--- TaskManagement/App_Start/UnityConfig.cs
+++ TaskManagement/App_Start/UnityConfig.cs
@@ -1,7 +1,9 @@
 using System.Web.Http;
 using System.Web.Mvc;
+using TaskManagement.Controllers;
 using TaskManagement.Models.Application;
 using Unity;
+using Unity.Injection;
 using Unity.Mvc5;
 
 namespace TaskManagement
@@ -13,6 +15,7 @@ namespace TaskManagement
 			var container = new UnityContainer();
 
             container.RegisterType<IApplicationDbContext, ApplicationDbContext>();
+            container.RegisterType<AccountController>(new InjectionConstructor());
 
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
             GlobalConfiguration.Configuration.DependencyResolver = new Unity.WebApi.UnityDependencyResolver(container);
